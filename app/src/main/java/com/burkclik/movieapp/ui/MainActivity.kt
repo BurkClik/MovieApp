343c3eab16e92a751fec299a9aa9a45c0f05e162
BurkClik <burk.clik@gmail.com>
2021-05-19 18:28:50 +0300

    initial commit

---
+++ app/src/main/java/com/burkclik/movieapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.burkclik.movieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.burkclik.movieapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
