d22fc4b55316281ea3856d786e207b86d36e0911
BurkClik <burk.clik@gmail.com>
2021-05-27 00:44:23 +0300

    Add caching.

--- app/src/main/java/com/burkclik/movieapp/di/AppModule.kt
+++ app/src/main/java/com/burkclik/movieapp/di/AppModule.kt
@@ -1,11 +1,14 @@
 package com.burkclik.movieapp.di
 
+import android.app.Application
 import com.burkclik.movieapp.BASE_URL
 import com.burkclik.movieapp.api.MovieApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -16,13 +19,26 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun providerRetrofit(): Retrofit = Retrofit.Builder()
+    fun providerRetrofit(client: OkHttpClient): Retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
         .build()
 
     @Provides
     fun provideApi(retrofit: Retrofit): MovieApi {
         return retrofit.create(MovieApi::class.java)
     }
+
+    @Provides
+    fun provideCache(application: Application): Cache {
+        val cacheSize = 10 * 1024 * 1024
+        return Cache(application.cacheDir, cacheSize.toLong())
+    }
+
+    @Provides
+    fun provideHttpClient(cache: Cache): OkHttpClient =
+        OkHttpClient.Builder()
+            .cache(cache)
+            .build()
 }
