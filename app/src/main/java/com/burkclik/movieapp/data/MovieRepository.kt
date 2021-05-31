f0b0693bccd0312604921f65feb2fbb717abc089
BurkClik <burk.clik@gmail.com>
2021-05-31 04:00:40 +0300

    Move requests to repository from use cases.

--- app/src/main/java/com/burkclik/movieapp/data/MovieRepository.kt
+++ app/src/main/java/com/burkclik/movieapp/data/MovieRepository.kt
@@ -1,32 +1,70 @@
 package com.burkclik.movieapp.data
 
-import android.util.Log
+import com.burkclik.movieapp.common.Result
 import com.burkclik.movieapp.data.remote.MovieDataSource
 import com.burkclik.movieapp.data.remote.model.CastResponse
-import com.burkclik.movieapp.data.remote.model.MovieDetail
-import com.burkclik.movieapp.data.remote.model.MovieResponse
+import com.burkclik.movieapp.data.remote.model.MovieDetailResponse
+import com.burkclik.movieapp.data.remote.model.MovieListResponse
 import kotlinx.coroutines.CoroutineDispatcher
-import retrofit2.Response
 import javax.inject.Inject
 
 class MovieRepository @Inject constructor(
     private val movieDataSource: MovieDataSource,
     private val dispatcher: CoroutineDispatcher
 ) {
-    suspend fun getTheaterMovies(): MovieResponse {
-        return movieDataSource.getInTheaterMovies()
+    suspend fun getTheaterMovies(): Result<MovieListResponse?> {
+        return try {
+            val result = movieDataSource.getInTheaterMovies()
+            if (result.isSuccessful) {
+                val movies = result.body()
+                Result.Success(movies)
+            } else {
+                Result.Error(result.code().toString())
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
     }
 
-    suspend fun getMovieDetail(movieId: Int): Response<MovieDetail> {
-        return movieDataSource.getMovieDetail(movieId)
+    suspend fun getMovieDetail(movieId: Int): Result<MovieDetailResponse?> {
+        return try {
+            val result = movieDataSource.getMovieDetail(movieId)
+            if (result.isSuccessful) {
+                val movie = result.body()
+                Result.Success(movie)
+            } else {
+                Result.Error("Unexpected Error", status = result.code())
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
     }
 
-    suspend fun getRelatedMovies(movieId: Int): Response<MovieResponse> {
-        Log.i("Burak", "Repository -> ${Thread.currentThread().name}")
-        return movieDataSource.getRelatedMovies(movieId)
+    suspend fun getRelatedMovies(movieId: Int): Result<MovieListResponse?> {
+        return try {
+            val result = movieDataSource.getRelatedMovies(movieId)
+            if (result.isSuccessful) {
+                val movies = result.body()
+                Result.Success(movies)
+            } else {
+                Result.Error(result.code().toString())
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
     }
 
-    suspend fun getCreditsMovie(movieId: Int): CastResponse {
-        return movieDataSource.getMovieCredits(movieId)
+    suspend fun getCreditsMovie(movieId: Int): Result<CastResponse?> {
+        return try {
+            val result = movieDataSource.getMovieCredits(movieId)
+            if (result.isSuccessful) {
+                val credits = result.body()
+                Result.Success(credits)
+            } else {
+                Result.Error(result.code().toString())
+            }
+        } catch (e: Exception) {
+            Result.Error(e.message.toString())
+        }
     }
 }
