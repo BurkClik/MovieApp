9cd2693a0fde2fbf7f76ded1f03b1648e3e39452
BurkClik <burk.clik@gmail.com>
2021-05-31 03:58:26 +0300

    Add url for high res images.

--- app/src/main/java/com/burkclik/movieapp/common/Constants.kt
+++ app/src/main/java/com/burkclik/movieapp/common/Constants.kt
@@ -3,3 +3,4 @@ package com.burkclik.movieapp.common
 const val API_KEY = "86b3f3a29af01e1de22fbcbf43d20947"
 const val BASE_URL = "https://api.themoviedb.org/"
 const val IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500/"
+const val HIGH_RES_IMAGE_URL = "https://image.tmdb.org/t/p/original/"
